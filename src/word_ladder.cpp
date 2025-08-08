b03af196c9535635669f022b70eb791918ca5d9f
Paul <lecomtepaulvd@gmail.com>
2025-08-08 09:15:52 +0200

    helper function word ladder

--- src/word_ladder.cpp
+++ src/word_ladder.cpp
@@ -2,7 +2,13 @@
 #include <queue>
 #include <unordered_map>
 
+// helper function for findWordLadder
+bool differByOne() {
+
+};
+
 std::vector<std::string> findWordLadder(const std::string& start, const std::string& end, const std::unordered_set<std::string>& dict) {
     // TODO: Implémenter l’algorithme BFS
+
     return {}; // stub
 }
