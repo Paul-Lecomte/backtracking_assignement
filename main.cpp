40f89c7cc7d7f53a538f99399d0f7dc02e348015
Paul <lecomtepaulvd@gmail.com>
2025-08-08 09:45:28 +0200

    load dictionary helper

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <unordered_set>
 #include "word_ladder.hpp"
 #include "maze_solver.hpp"
 #include "boggle_solver.hpp"
+
+// Helper to load dictionary from file
+std::unordered_set<std::string> loadDictionary(const std::string& filename) {
+    std::unordered_set<std::string> dict;
+    std::ifstream file(filename);
+    std::string word;
+    while (file >> word) {
+        dict.insert(word);
+    }
+    return dict;
+}
 
 int main() {
     std::cout << "Assignment 4 - Backtracking Problems" << std::endl;
