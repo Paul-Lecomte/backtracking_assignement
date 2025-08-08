51d0b70d0fac6f790510da8a91a24d952909318e
Paul <lecomtepaulvd@gmail.com>
2025-08-08 09:26:21 +0200

    helper function word ladder

--- src/word_ladder.cpp
+++ src/word_ladder.cpp
@@ -3,8 +3,15 @@
 #include <unordered_map>
 
 // helper function for findWordLadder
-bool differByOne() {
-
+bool differByOne(const std::string& a, const std::string& b) {
+    int diffcount = 0;
+    for (size_t i = 0; i< a.size(); i++) {
+        if (a[i] != b[i]) {
+            diffcount++;
+            if (diffcount > 1) return false; // More than one character differs
+        }
+    }
+    return diffcount == 1;
 };
 
 std::vector<std::string> findWordLadder(const std::string& start, const std::string& end, const std::unordered_set<std::string>& dict) {
