9472bc1dca36a2c9e542d9325e608b47654ee86a
Paul <lecomtepaulvd@gmail.com>
2025-08-10 09:12:17 +0200

    helper function start

--- src/maze_solver.cpp
+++ src/maze_solver.cpp
@@ -1,4 +1,14 @@
 #include "maze_solver.hpp"
+#include <vector>
+
+// Helper function for recursion
+bool backtrack(const Maze& maze,
+               std::pair<int,int> current,      // where we are now
+               std::pair<int,int> end,          // goal coordinates
+               std::vector<std::vector<bool>>& visited, // visited tracker
+               Path& path) {
+
+}
 
 Path solveMaze(const Maze& maze, std::pair<int, int> start, std::pair<int, int> end) {
     // TODO: Backtracking récursif pour trouver un chemin
