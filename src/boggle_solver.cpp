075fc48a7a6d1bff5a330b74f2b8ced07e0917ec
Paul <lecomtepaulvd@gmail.com>
2025-08-13 09:07:02 +0200

    loop

--- src/boggle_solver.cpp
+++ src/boggle_solver.cpp
@@ -3,6 +3,21 @@
 #include <cctype>
 #include <string>
 
+namespace {
+    constexpr int MIN_WORD_LEN = 3;
+
+    // Build a set of all possible prefixes inn the dictionary
+    std::unordered_set<std::string> buildPrefixSet(const std::unordered_set<std::string>& dict) {
+        std::unordered_set<std::string> prefixes;
+        for (const auto& word : dict) {
+            for (size_t len = 1; len <= word.size(); ++len) {
+                prefixes.insert(word.substr(0, len));
+            }
+        }
+        return prefixes;
+    }
+}
+
 std::unordered_set<std::string> findAllWords(const Board& board, const std::unordered_set<std::string>& dictionary) {
     // TODO: DFS + backtracking
     return {}; // stub
