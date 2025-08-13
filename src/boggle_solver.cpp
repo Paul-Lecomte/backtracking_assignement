6089a6ab68324af41cfaef2b11c9e9908b4a213a
Paul <lecomtepaulvd@gmail.com>
2025-08-13 08:47:59 +0200

    imports

--- src/boggle_solver.cpp
+++ src/boggle_solver.cpp
@@ -1,4 +1,7 @@
 #include "boggle_solver.hpp"
+#include <array>
+#include <cctype>
+#include <string>
 
 std::unordered_set<std::string> findAllWords(const Board& board, const std::unordered_set<std::string>& dictionary) {
     // TODO: DFS + backtracking
